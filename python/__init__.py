d197de6db0063ea1b37d879a93874fbe181de370
ghostop14 <ghostop14@gmail.com>
2019-10-17 14:25:28 -0400

    Added Variable to Message Block

--- python/__init__.py
+++ python/__init__.py
@@ -2,4 +2,5 @@ from .doppler import doppler
 from .rotor import rotor
 from .azel_limit import AzElLimit
 from .MsgPairToVar import MsgPairToVar
+from .vartomsg import VarToMsgPair
 from .vel_doppler import vel_doppler
---
+++ python/vartomsg.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# 
+
+from gnuradio import gr
+import pmt
+
+class VarToMsgPair(gr.sync_block):
+  def __init__(self, pairname):
+    gr.sync_block.__init__(self, name = "VarToMsg", in_sig = None, out_sig = None)
+    
+    self.pairname = pairname
+             
+    self.message_port_register_out(pmt.intern("msgout"))
+
+  def variableChanged(self, value):
+    if (type(value) == float):
+        p = pmt.from_float(value)
+    elif (type(value) == int):
+        p = pmt.from_long(value)
+    elif (type(value) == bool):
+        p = pmt.from_bool(value)
+    else:
+        p = pmt.intern(value)
+        
+    self.message_port_pub(pmt.intern("msgout"),pmt.cons(pmt.intern(self.pairname),p))
+    
+  def stop(self):
+    return True
