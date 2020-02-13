7cba399f07eb82de42d4c28940ea89db99107bc0
ghostop14 <ghostop14@gmail.com>
2020-02-13 09:19:58 -0500

    Cleaned up class and docs

--- python/MsgPairToVar.py
+++ python/MsgPairToVar.py
@@ -5,22 +5,25 @@ from gnuradio import gr
 import pmt
 
 class MsgPairToVar(gr.sync_block):
-  def __init__(self, callback):
-    gr.sync_block.__init__(self, name = "MsgPairToVar", in_sig = None, out_sig = None)
-    
-    self.callback = callback
-             
-    self.message_port_register_in(pmt.intern("inpair"))
-    self.set_msg_handler(pmt.intern("inpair"), self.msgHandler)   
+    """
+    This block will take an input message pair and allow you to set a gnuradio variable.
+    """
+    def __init__(self, callback):
+        gr.sync_block.__init__(self, name="MsgPairToVar", in_sig=None, out_sig=None)
 
-  def msgHandler(self, msg):
-    try:    
-      newVal = pmt.to_python(pmt.cdr(msg))
-      
-      self.callback(newVal)
-      
-    except Exception as e:
-      print("[MsgPairToVar] Error with message conversion: %s" % str(e))
-    
-  def stop(self):
-    return True
+        self.callback = callback
+
+        self.message_port_register_in(pmt.intern("inpair"))
+        self.set_msg_handler(pmt.intern("inpair"), self.msg_handler)
+
+    def msg_handler(self, msg):
+        try:
+            new_val = pmt.to_python(pmt.cdr(msg))
+
+            self.callback(new_val)
+
+        except Exception as e:
+            gr.log.error("Error with message conversion: %s" % str(e))
+
+    def stop(self):
+        return True
